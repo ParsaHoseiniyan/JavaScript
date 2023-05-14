f7186efd76d9dcccda3a9076cafeccafe364971d
Parsa <Parsa16.pmh@gmail.com>
2023-05-14 21:53:59 +0430

    logical,bitwise operators' introductions added

---
+++ logical,bitwise operators/script.js
@@ -0,0 +1,19 @@
+let a = 3,
+    b = 4;
+
+//logical operators
+
+console.log(a == b); //is equal?  false
+console.log(a != b); //is not equal? true
+console.log(a > b); //is a greater than b? false
+console.log(a < b); //is a less than b? true
+console.log(a === b); //is the value and the typeof of a and b the same? true
+
+//bitwise operator
+/*
+
+  a=3 || b=0   //OR: atleast one of them should be true so the statement be true
+  a=3 && b=0   //AND: both of themshould be true so the statement be true
+
+
+*/
