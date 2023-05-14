99e39382f3616788c6e77aa842eb2f0292a8be5f
Parsa <Parsa16.pmh@gmail.com>
2023-05-14 21:48:32 +0430

    ses 13 and 14 files added

---
+++ ses-13(is NaN method)/script.js
@@ -0,0 +1,8 @@
+// Working with the isNaN method: you can simply understand that if sth is NaN or not in your source code.
+// resources: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isNaN
+
+/* 1 tip: we use () after every method */
+
+// isNaN gives us true or false
+alert(isNaN(str1 / num1)) // true
+
---
+++ ses-14(comparative operators)/script.js
@@ -0,0 +1,11 @@
+// session 14: comparative operators
+
+
+// == , < , > , >= , =< , ===
+
+// what is the differce between == and === ?
+// == have nothing to do with the type of the variables and it just compares the values
+
+// but when we use === , in the first step it checks the type of the variables
+
+alert("12" === 12) // => false
