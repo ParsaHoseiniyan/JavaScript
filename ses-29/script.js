d5093a23bc0e7933d6667bc5d18c0f3b5dad0915
Parsa <Parsa16.pmh@gmail.com>
2023-05-21 11:33:00 +0430

    session 29 added

---
+++ ses-29/script.js
@@ -0,0 +1,12 @@
+// Function Declaration
+
+// how to define a function as declaration sintax
+function showValue () {
+    num1 = 2
+    num2 = 8
+    resultNumber = num1 + num2
+    alert('Result: ' + resultNumber)
+}
+
+// how to call a declaration function:
+showValue()
