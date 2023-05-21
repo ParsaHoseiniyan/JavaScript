38261f0ea7996c83794a209aedfdd2b7e32cf248
Parsa <Parsa16.pmh@gmail.com>
2023-05-21 12:07:35 +0430

    session 30 added

---
+++ ses-30/script.js
@@ -0,0 +1,10 @@
+// Function expressions
+// https://javascript.info/function-basics
+// how to define a function as expressions syntax(just a little different from declarations)
+
+var showValue = function() {
+    var num1 = 2
+    var num2 = 8
+    var result = num1 + num2
+    alert('the result: ' + result)
+}
