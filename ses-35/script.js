214070d09b924de9221b7891d9d82fcaab3e08a1
Parsa <Parsa16.pmh@gmail.com>
2023-05-23 13:11:34 +0430

    codes added

--- ses-35/script.js
+++ ses-35/script.js
@@ -1,17 +1,25 @@
-// Function Parameters
-// https://javascript.info/function-basics
+// role of the return keyword in javascript
+// there are two type of this function you see down below 
+// there is just one difference in these two functions that the second function doesn't have the 12th line in the first function
+// resource: https://javascript.info/function-basics#returning-a-value 
 
-// alert('من یه آلرت هستم')
+var number1 = Number(prompt('first number: '))
+var number2 = Number(prompt('second number: '))
 
-function showText (userText) {
-	alert("Your Parameter: " + userText)
+function showpower (num1, num2) {
+	return num1 ** num2
 }
 
-// showText('SabzLearn :)')
+var resultOfFunction = showpower(number1, number2)
+alert('power result: ' + resultOfFunction)
 
-function showTwoNumbers (firstNumber, secondNumber) {
-	alert("First Parameter: " + firstNumber)
-	alert("Second Parameter: " + secondNumber)
+////////////////////////////////////////////////
+
+var Number1 = Number(prompt('first number: '))
+var Number2 = Number(prompt('second number: '))
+
+function power (x, y) {
+	return x ** y
 }
 
-showTwoNumbers(34, 18)
+alert("your second power result: " + power(Number1, Number2))
