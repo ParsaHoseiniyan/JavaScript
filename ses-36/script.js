385f91c1b374c7e32544e4430d7752456a60d115
Parsa <Parsa16.pmh@gmail.com>
2023-05-23 20:24:31 +0430

    session 36 added

---
+++ ses-36/script.js
@@ -0,0 +1,12 @@
+// string structures and properties
+// resource: https://www.w3schools.com/jsref/jsref_obj_string.asp
+
+var userName = 'js' 
+
+var firstIndex = userName[0]
+
+// alert(firstIndex) // index
+
+// alert(userName.length)
+
+alert(userName[4])
