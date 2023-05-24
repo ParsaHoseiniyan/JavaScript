fd1e08016e1a050754a2853da966a91dd8d67faa
Parsa <Parsa16.pmh@gmail.com>
2023-05-24 23:52:36 +0430

    session 37 done

--- ses-37/script.js
+++ ses-37/script.js
@@ -1,1 +1,11 @@
-// string methods-1
+// exercise for session 36 is done in session 37(here)
+// this exercise is about making a registor form that user needs to add a username in atleast 3 charachters and a password with atleast 8 charachters
+
+var userName = prompt("add a username for your account: ")
+var userPass = prompt('add a password for your account: ')
+
+if (userName.length < 3 || userPass.length < 8) {
+    alert('you can not registor because your username should be at least 3 charachters or your password should be atleast 8!')
+} else {
+    alert('successfully added!')
+}
