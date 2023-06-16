289dc0c7b42597696692ab6306296a111825b01b
Parsa <Parsa16.pmh@gmail.com>
2023-06-16 09:39:07 +0330

    folder 92 to 100 added

---
+++ Sessions/ses-16 - Copy (5)/script.js
@@ -0,0 +1,14 @@
+// session 16: exercise 2
+// here I do the exercises for session 16
+
+var num1 = prompt("type the first number:")
+var num2 = prompt("type the 2nd number:")
+var num3 = prompt("type the 3rd number:")
+
+var firstNumber = Number(num1)
+var secondNumber = Number(num2)
+var thirdNumber = Number(num3)
+
+var average = (firstNumber + secondNumber + thirdNumber) / 3
+
+alert(`your average number is : \n ${average}`)
