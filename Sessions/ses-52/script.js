0cff13ea4553c73347e469f233fc6c361cd0140e
Parsa <Parsa16.pmh@gmail.com>
2023-06-16 14:11:44 +0330

    exercise in session 52 done

--- Sessions/ses-52/script.js
+++ Sessions/ses-52/script.js
@@ -1,17 +1,9 @@
-// Function Parameters
-// https://javascript.info/function-basics
-
-// alert('من یه آلرت هستم')
-
-function showText (userText) {
-	alert("Your Parameter: " + userText)
+// exercise: 
+var userNumber = Number(prompt('enter a number: '))
+var counter = 0
+for (var i = 0; userNumber / 10 != 0; i++) {
+	counter++
+	userNumber = Math.floor(userNumber / 10)
 }
 
-// showText('SabzLearn :)')
-
-function showTwoNumbers (firstNumber, secondNumber) {
-	alert("First Parameter: " + firstNumber)
-	alert("Second Parameter: " + secondNumber)
-}
-
-showTwoNumbers(34, 18)
+console.log(counter)
