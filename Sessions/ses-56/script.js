57877a37dbcafeaedaa107789b8ac932190fbd46
Parsa <Parsa16.pmh@gmail.com>
2023-06-16 19:18:05 +0330

    session 56 exercise done

--- Sessions/ses-56/script.js
+++ Sessions/ses-56/script.js
@@ -1,17 +1,15 @@
-// Function Parameters
-// https://javascript.info/function-basics
+//exercise for while-loop(the purpose of this exercise is that you know that you wll not have the stop condition in
+// loops we have to write the stop condition by ourselves.)
+var userNumber = 0
+var sum = 0
+var counter = 0
 
-// alert('من یه آلرت هستم')
-
-function showText (userText) {
-	alert("Your Parameter: " + userText)
+while (userNumber >= 0) {
+	sum = sum + userNumber // => first it should plus sum to userNumber and after that get a new number from user
+	userNumber = Number(prompt('Enter The Number: \n if you dont have a number, enter a negetive number'))
+	if (userNumber >= 0) {
+		counter++
+	}
 }
-
-// showText('SabzLearn :)')
-
-function showTwoNumbers (firstNumber, secondNumber) {
-	alert("First Parameter: " + firstNumber)
-	alert("Second Parameter: " + secondNumber)
-}
-
-showTwoNumbers(34, 18)
+// console.log("Sum: " + sum + "\n" + "counter: " + counter)
+console.log("Average: " + sum / counter)
