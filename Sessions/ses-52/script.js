b564a1fd96df4b684356d0b8d78243e6d8332c16
Parsa <Parsa16.pmh@gmail.com>
2023-06-16 16:05:59 +0330

    session 52 exercise done

--- Sessions/ses-52/script.js
+++ Sessions/ses-52/script.js
@@ -1,9 +1,22 @@
 // exercise: 
 var userNumber = Number(prompt('enter a number: '))
 var counter = 0
-for (var i = 0; userNumber / 10 != 0; i++) {
-	counter++
-	userNumber = Math.floor(userNumber / 10)
+
+if (isNaN(userNumber) == false) {
+
+	if (userNumber == 0) {
+		counter = 1
+	} else {
+		for (var i = 0; userNumber / 10 != 0; i++) {
+			counter++
+			userNumber = Math.floor(userNumber / 10)
+		}
+		
+	}
+	alert(counter)
+} else {
+	alert('faaaaaalse! It should be a number!')
 }
 
-console.log(counter)
+
+
